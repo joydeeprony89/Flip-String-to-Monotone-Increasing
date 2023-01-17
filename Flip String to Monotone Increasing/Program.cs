cf8bcc2f38c43d065645da5f7b81635a86d938b9
jd_786 <jaydeep_shil@certree.com>
2023-01-17 12:11:35 +0530

    Add project files.

---
+++ Flip String to Monotone Increasing/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// https://www.youtube.com/watch?v=XokGEheMf5Y
+
+public class Solution
+{
+  public int MinFlipsMonoIncr(string s)
+  {
+    var zeroCount = 0;
+    var oneCount = 0;
+    foreach (char c in s) if (c == '0') zeroCount++;
+    int ans = zeroCount;
+    foreach (char c in s)
+    {
+      if (c == '0') zeroCount--;
+      else if (c == '1') oneCount++;
+      ans = Math.Min(ans, zeroCount + oneCount);
+    }
+    return ans;
+  }
+}
